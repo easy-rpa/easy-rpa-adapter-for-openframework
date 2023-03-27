638f10530ab945446cfdbdfccf200767a291bf0b
Mikhail Trubach <mtrubatch@ibagroup.eu>
2023-03-27 17:17:23 +0300

    Implementation for Core version 1.1.0

--- src/main/java/eu/ibagroup/easyrpa/engine/service/impl/EasyRPAServicesAccessor.java
+++ src/main/java/eu/ibagroup/easyrpa/engine/service/impl/EasyRPAServicesAccessor.java
@@ -1,10 +1,20 @@
 package eu.ibagroup.easyrpa.engine.service.impl;
 
-import eu.ibagroup.easyrpa.engine.service.ConfigurationService;
-import eu.ibagroup.easyrpa.engine.service.VaultService;
+import eu.easyrpa.openframework.core.model.FileData;
 import eu.easyrpa.openframework.core.sevices.RPAServicesAccessor;
+import eu.ibagroup.easyrpa.engine.service.ConfigurationService;
+import eu.ibagroup.easyrpa.engine.service.NotificationService;
+import eu.ibagroup.easyrpa.engine.service.VaultService;
 
 import javax.inject.Inject;
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Consumer;
 
 public class EasyRPAServicesAccessor implements RPAServicesAccessor {
 
@@ -13,6 +23,9 @@ public class EasyRPAServicesAccessor implements RPAServicesAccessor {
 
     @Inject
     private VaultService vaultService;
+
+    @Inject
+    private NotificationService notificationService;
 
     @Override
     public String getConfigParam(String key) {
@@ -23,4 +36,43 @@ public class EasyRPAServicesAccessor implements RPAServicesAccessor {
     public <T> T getSecret(String alias, Class<T> cls) {
         return vaultService.getSecret(alias, cls);
     }
+
+    @Override
+    public void sendMessage(String channelName, String templateName, Map<String, ?> params, List<? extends FileData> files) {
+        prepareTempFilesAndRun(files, (tempFiles) -> {
+            notificationService.evaluateTemplateAndSend(templateName, params, channelName, tempFiles);
+        });
+    }
+
+    @Override
+    public void sendMessage(String channelName, String subject, String content, List<? extends FileData> files) {
+        prepareTempFilesAndRun(files, (tempFiles) -> {
+            notificationService.sendNotification(channelName, content, subject, tempFiles);
+        });
+    }
+
+    private void prepareTempFilesAndRun(List<? extends FileData> files, Consumer<List<File>> action) {
+        Path tempDir = null;
+        try {
+            tempDir = Files.createTempDirectory("files_to_send");
+            List<File> tempFiles = new ArrayList<>();
+            for (FileData fileData : files) {
+                Path tempFile = Files.createFile(Paths.get(tempDir.toFile().getAbsolutePath(), fileData.getFileName()));
+                Files.write(tempFile, fileData.getContent());
+                tempFiles.add(tempFile.toFile());
+            }
+            action.accept(tempFiles);
+        } catch (Exception e) {
+            throw new RuntimeException("Preparing of files to send has failed.", e);
+        } finally {
+            try {
+                if (tempDir != null) {
+                    Files.delete(tempDir);
+                }
+            } catch (Exception e) {
+                //do nothing
+            }
+        }
+    }
+
 }
